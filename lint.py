71704980043ab1ce15dd6b0cbe98e0f583559b58
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-14 09:15:10 +0200

    Update metadata and annotations

--- lint.py
+++ lint.py
@@ -2,22 +2,34 @@
 # requires-python = ">=3.14"
 # dependencies = [
 #     "pandas>=3.0.3",
+#     "click",
 # ]
 # ///
 
 import sys
 from pathlib import Path
 
+import click
 import pandas as pd
 
 HERE = Path(__file__).parent.resolve()
 
-if __name__ == "__main__":
+
+@click.command()
+@click.option("--fix", is_flag=True)
+def main(fix: bool) -> None:
     fail = False
     for path in HERE.glob("*.tsv"):
         original = path.read_text()
-        corrected = pd.read_csv(path, sep="\t").to_csv(sep="\t", index=False)
+        actual = pd.read_csv(path, sep="\t")
+        if fix:
+            actual.to_csv(path, index=False, sep="\t")
+        corrected = actual.to_csv(sep="\t", index=False)
         if original != corrected:
             print(f"TSV not formatted properly: {path}")
             fail = True
     sys.exit(1 if fail else 0)
+
+
+if __name__ == "__main__":
+    main()
